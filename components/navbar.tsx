e90cddc5c659ed11433b585e630e1e7a9bd91c97
amintai <amintai157@gmail.com>
2024-03-30 16:25:41 +0530

    Add Smooth Scroll From Navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -10,7 +10,7 @@ import { useState } from "react";
 export default function Navbar() {
   const router = useRouter();
   const [{ pathName }, setState] = useState({
-    pathName: '/'
+    pathName: 'home'
   });
 
   function navigateTo(path: string) {
@@ -18,56 +18,60 @@ export default function Navbar() {
       ...state,
       pathName: path
     }))
-    router.push(path);
+
+    window.scrollTo({
+      top: document.getElementById(path).offsetTop - 60,
+      behavior: 'smooth',
+    });
   }
 
 
   return (
     <div className="border bg-card text-card-foreground p-3 shadow-sm md:rounded-lg flex justify-between items-center top-0 sticky z-50">
-      <Logo onClick={() => navigateTo("/")} />
+      <Logo onClick={() => navigateTo("home")} />
       {/* Navigation Menus */}
       <div className="space-x-3 items-center">
         <Button
-          onClick={() => navigateTo("/")}
+          onClick={() => navigateTo("home")}
           variant="outline"
           size="sm"
-          className={`ml-auto h-8 ${pathName === '/' ? 'bg-black text-white' : ''} `}
+          className={`ml-auto h-8 ${pathName === 'home' ? 'bg-black text-white' : ''} `}
         >
           <Home className=" h-4 w-4" />
           <span className="hidden md:block ml-2">Home</span>
         </Button>
         <Button
-          onClick={() => navigateTo("/#projects")}
+          onClick={() => navigateTo("projects",)}
           variant="outline"
           size="sm"
-          className={`ml-auto h-8 ${pathName === '/#projects' ? 'bg-black text-white' : ''} `}
+          className={`ml-auto h-8 ${pathName === 'projects' ? 'bg-black text-white' : ''} `}
         >
           <Presentation className=" h-4 w-4" />
           <span className="hidden md:block ml-2">Projects</span>
         </Button>
         <Button
-          onClick={() => navigateTo("/#work")}
+          onClick={() => navigateTo("work")}
           variant="outline"
           size="sm"
-          className={`ml-auto h-8 ${pathName === '/#work' ? 'bg-black text-white' : ''} `}
+          className={`ml-auto h-8 ${pathName === 'work' ? 'bg-black text-white' : ''} `}
         >
           <Briefcase className=" h-4 w-4" />
           <span className="hidden md:block ml-2">Work</span>
         </Button>
         <Button
-          onClick={() => navigateTo("/#blogs")}
+          onClick={() => navigateTo("blogs")}
           variant="outline"
           size="sm"
-          className={`ml-auto h-8 ${pathName === '/#blogs' ? 'bg-black text-white' : ''} `}
+          className={`ml-auto h-8 ${pathName === 'blogs' ? 'bg-black text-white' : ''} `}
         >
           <BookMarked className=" h-4 w-4" />
           <span className="hidden md:block ml-2">Blogs & Achievements</span>
         </Button>
         <Button
-          onClick={() => navigateTo("/#contact")}
+          onClick={() => navigateTo("contact")}
           variant="outline"
           size="sm"
-          className={`ml-auto h-8 ${pathName === '/#contact' ? 'bg-black text-white' : ''} `}
+          className={`ml-auto h-8 ${pathName === 'contact' ? 'bg-black text-white' : ''} `}
         >
           <Mail className=" h-4 w-4" />
           <span className="hidden md:block ml-2">Contact</span>
